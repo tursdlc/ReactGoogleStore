581beaac3c0332afe5a3624dc25b70eacb94d6f6
gabriela <gabrielalacueva@gmail.com>
2024-06-13 15:31:19 +0200

    Cambio estructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 
 import Nav from "./components/Nav.jsx";
 import "./App.css"
-
+import Main from "./components/main-container.jsx";
 import "./components/Nav.css"
+import "./components/main-container.css"
 
 
 
 const App = () =>{
   return (
     <>
+  <div className="Nav">
    <Nav></Nav>
+   </div>
+   <div className="body">
+   <Main></Main>
+   
+   </div>
+   <div className="Footer">
+
+   </div>
     </>
   )
 
