88b179e9a0fc00a3f3aaebece9ac48fc1632185b
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-06-14 10:07:08 +0200

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@
 import Nav from "./components/Nav.jsx";
 import "./App.css"
 import Main from "./components/main-container.jsx";
+import Addtocart from "./components/Addtocart.jsx";
 import "./components/Nav.css"
 import "./components/main-container.css"
+import "./components/Addtocart.css"
+
+
 
 
 
@@ -11,15 +15,15 @@ const App = () =>{
   return (
     <>
   <div className="Nav">
-   <Nav></Nav>
-   </div>
-   <div className="body">
-   <Main></Main>
-   
-   </div>
-   <div className="Footer">
+<Nav></Nav>
+</div>
+<div className="body">
+<Main></Main>
+<Addtocart></Addtocart>
+</div>
+<div className="Footer">
 
-   </div>
+</div>
     </>
   )
 
---
+++ src/components/Addtocart.jsx
@@ -0,0 +1,19 @@
+function Addtocart(){
+    return (
+        <>
+        <div className="Addtocart">
+        <div className="Price">
+        <h1> 99.95€</h1>  
+        </div>
+
+        <div className="Button">
+            <button>Add To Cart</button>
+        </div>
+        </div>
+        </>
+    )
+}
+
+
+
+export default Addtocart
